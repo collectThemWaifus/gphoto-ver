64f297722c29f6fc4daeab4b37c7ff3c15ca62c4
Sangmin Chun <sangmin@sagg.in>
2025-01-01 19:51:37 -0800

    More boilerplate code

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 # This example requires the 'message_content' intent.
 from os import environ
 
+from commands.Roll import Roll
 import discord
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+
+        self.bot.add_cog (Roll)
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
